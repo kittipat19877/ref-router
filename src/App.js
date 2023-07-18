878b711be2cd39cc096efcab0196e2aea93ed263
kittipat19877 <number8671@gmail.com>
2023-07-18 12:11:04 +0700

    create react ref-route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Button,  } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import Contact from './Contact';
+import { useRef } from 'react';
 
+export function Layout() {
+  return (
+  <nav className='bg-secondary p-2 mb-3 text-center'>
+    <NavLink to="/" className="link px-2" 
+    style={({ isActive }) => {
+      return {
+          textDecoration: isActive ? "none" : "underline",
+      };
+    }}
+    >
+    Main
+    </NavLink>
+    <NavLink 
+    to="/product " 
+    className="link px-2"
+    >Product</NavLink>
+  </nav>
+  );
+}
+function Index() {
+  const header1 = useRef();
+  return (
+    <>
+    <Layout />
+    <h3 ref={header1}>Hello</h3>
+    <Button onClick={()=>{header1.current.innerHTML = "React";
+  }}
+  >
+    Click Me</Button>
+    </>
+  );
+}
+function Product() {
+  return (
+    <>
+    <Layout />
+    <h4>Product</h4>
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Container className='p-3 my-3 bg-danger'>
+    <Routes>
+      <Route path='/' element={<Index />} />
+      <Route path='/main' element={<Index />} />
+      <Route path='/product' element={<Product />} />
+      <Route path='/contact' element={<Contact/>} /> 
+
+    </Routes>
+    </Container>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Layout } from "./App";
+
+function Contact() {
+return (
+<>
+<Layout />
+<h3>Ubon Racthatani University</h3>
+<div>
+    85 Sathonlamark road<br />
+    Warin chamrap Distric<br />
+    Ubon Racthatani<br />
+    Thailand 34190<br />
+</div>
+</>
+);
+}
+
+export default Contact;
